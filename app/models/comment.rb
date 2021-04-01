2f5e8b8e072d2f3a72cad2be6ddda5ac6d91cfc9
zooootech <yzmisng@gmail.com>
2021-04-01 23:52:35 +0900

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   # userおよびprototypeに対するアソシエーションを設定
   belongs_to :user
   belongs_to :prototype
+  
   # バリデーションを設定し、保存する情報に制限をかける
   validates :text, presence: true
 end
